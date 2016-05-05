eb04ee645ce6ebf36582ce93dd3a8d1f7df2ea5d
albert <albertxerta93@hotmail.com>
2016-05-05 21:10:55 +0200

    some page styles

--- resources/views/games/infiniterunner.blade.php
+++ resources/views/games/infiniterunner.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.app')
 
-@section('htmlheader_title')
-	Infinite runner
-@endsection
-
 
 @section('main-content')
 <script type="text/javascript" src={{ asset('/js/phaser.min.js') }}></script>
 <script type="text/javascript" src={{ asset('/js/infinite_runner.js') }}></script>
 
-  <h1>PLAY INFINITE RUNNER HERE</h1>
-
-<div id="infinite-runner"></div>
+<div id="infinite-runner" class="game-div"></div>
 
 @endsection
