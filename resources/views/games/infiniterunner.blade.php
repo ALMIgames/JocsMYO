23825a99db8fb66b1738c58bffec91e3eaf6a2df
albert <albertxerta93@hotmail.com>
2016-05-05 16:27:39 +0200

    added phaser js, infiniterunner js (with a clean template) and hardcoded js's in infiniteruniniterunner blade (TODO, solve this)

--- resources/views/games/infiniterunner.blade.php
+++ resources/views/games/infiniterunner.blade.php
@@ -6,7 +6,11 @@
 
 
 @section('main-content')
+<script type="text/javascript" src={{ asset('/js/phaser.min.js') }}></script>
+<script type="text/javascript" src={{ asset('/js/infinite_runner.js') }}></script>
 
   <h1>PLAY INFINITE RUNNER HERE</h1>
+
+
 
 @endsection
