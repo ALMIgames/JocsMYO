3c229c2777685f2dace2e2d9a8029f48d4d4b7bf
albert <albertxerta93@hotmail.com>
2016-05-12 17:03:35 +0200

    DJ. Creating blocs on a fixed position. Lateral collision on blocks deleted

--- public/js/doodle_jump.js
+++ public/js/doodle_jump.js
@@ -1,9 +1,7 @@
 var mainState = {
-//Funció de preload
+
   preload: function() {
 
-    //Carreguem els sprites
-    //game.load.image('player', '../img/infiniteplayer/player.png');
     game.load.spritesheet('player', '../img/doodleJump/player.png', 44, 65, 10);
     game.load.image('blocTop', '../img/infiniteRunner/grass.png');
     game.load.image('bloc', '../img/infiniteRunner/grassCenter.png');
@@ -12,56 +10,51 @@ var mainState = {
     game.load.audio('jump', ['../audio/infiniteRunner/jump.wav', '../audio/infiniteRunner/jump.mp3']);
   },
 
-//Funció de create
   create: function() {
-    //Puntuació
+
     this.score = 0;
     this.labelScore = game.add.text(20, 20, "0",
       { font: "30px Arial", fill: "#ffffff" });
 
-    //Definim el color del cel
     game.stage.backgroundColor = '#00BFFF';
 
-    //Carreguem les fisiques del phaser
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //Pintem el corredor
     this.player = game.add.sprite(200, 100, 'player');
 
-    //I li afegim la fisica
     game.physics.arcade.enable(this.player);
-
 
     var walk = this.player.animations.add('jump');
 
-
-    //Li afegim gravetat al corredor
     this.player.body.gravity.y = 1000;
 
-    //I fem que salti quan apretem l'espai
-    /*
-      NOTE:
-      PRIMER ESTIC FENT ELS JOCS EN TECLAT PER PODER TESTEJAR I PROVAR BE LES COSES.
-      IMPLEMENTAR CONTROLS AMB MYO ASAP
-    */
     cursors = game.input.keyboard.createCursorKeys();
 
-
-    //Creem un grup on afegirem mes endavant els blocs que formaran el terreny
     this.blocs = game.add.group();
 
+    this.timer = game.time.events.loop(1000, this.addRowOfBlocks, this);
 
-    //Per ultim afegim una capa plana de terreny per a començar la partida.
     for (var i = 0; i < 10; i++) {
         this.addOneBlockTop(i * 80, 450);
     }
+    for (var i = 0; i < 10; i++) {
+        this.addOneBlockTop(i * 80, 350);
+    }
+    for (var i = 0; i < 10; i++) {
+        this.addOneBlockTop(i * 80, 250);
+    }
+    for (var i = 0; i < 10; i++) {
+        this.addOneBlockTop(i * 80, 150);
+    }
+    for (var i = 0; i < 10; i++) {
+        this.addOneBlockTop(i * 80, 50);
+    }
 
-    //I carreguem el so
+
     this.jumpSound = game.add.audio('jump');
 
   },
 
-//Funció d'update
   update: function() {
 
     if (cursors.left.isDown) {
@@ -76,11 +69,9 @@ var mainState = {
     if (cursors.down.isDown) {
       console.log('down');
     }
-    //si el final del mapa atrapa el jugador, es eliminat i es reinicia el joc.
-    //mai hauria de sortir una casella per baix del mapa, pero per si de cas
-    //l'eliminem també si cau
+
     if (this.player.y > 490){
-      this.gameOver();
+      this.restartGame();
     }
 
     if (this.player.x < 0){
@@ -91,9 +82,7 @@ var mainState = {
       this.player.x = 0;
     }
 
-
     game.physics.arcade.collide(this.blocs, this.player);
-
 
     if(this.player.body.touching.down) {
         this.player.body.velocity.y = -500;
@@ -103,58 +92,70 @@ var mainState = {
   },
 
 
-//Funció de comprovació de salt (per al doble salt)
-  canJump: function() {
 
-  },
-
-//Funció de salt
-  jump: function() {
-
-  },
-
-//Funció pre crear el terreny
   addOneBlock: function(x, y) {
+    var bloc = game.add.sprite(x, y, 'bloc');
 
+    this.blocs.add(bloc);
+
+    game.physics.arcade.enable(bloc);
+
+    bloc.body.checkCollision.down = false;
+    bloc.body.checkCollision.left = false;
+    bloc.body.checkCollision.right = false;
+    this.blocs.setAll('body.immovable', true);
+
+    bloc.body.velocity.y = 100;
+
+    //Per acabar, quan un bloc surt de la pantalla el destruim
+    bloc.checkWorldBounds = true;
+    bloc.outOfBoundsKill = true;
   },
 
-//Funció pre crear el terreny (posició de dalt, amb herba)
   addOneBlockTop: function(x, y) {
-    //Crea un bloc de terreny a la posició x-y
     var blocTop = game.add.sprite(x, y, 'blocTop');
 
-    //Afegim blocs al grup
     this.blocs.add(blocTop);
 
-    //Li donem fisica al bloc
     game.physics.arcade.enable(blocTop);
 
     blocTop.body.checkCollision.down = false;
-    //I fem que sigui inamovible, ja que sino quan colisionaven es destrossava el mapa
+    blocTop.body.checkCollision.left = false;
+    blocTop.body.checkCollision.right = false;
     this.blocs.setAll('body.immovable', true);
 
-    //I una velocitat per moure'l cap a l'esquerra
-    //Nota: si augmentem la velocitat, per mantenir juntes les peces de terra
-    //hem de pujar la velocitat a la que cridem la funcio de crear
-    blocTop.body.velocity.x = 0;
+    blocTop.body.velocity.y = 100;
 
-    //Per acabar, quan un bloc surt de la pantalla el destruim
     blocTop.checkWorldBounds = true;
     blocTop.outOfBoundsKill = true;
   },
 
-//Funcio per crear columnes de blocs
   addRowOfBlocks: function() {
+    var space = Math.floor(Math.random() * 3 + 6);
+    var top = true;
 
+    for (var i = 0; i < 10; i++) {
+      if (i > space) {
+        if(top){
+          this.addOneBlockTop(50, 0);
+          top = false;
+        }
+        else{
+          this.addOneBlock(50, 0);
+        }
+      }
+    }
+
+    this.score += 1;
+    this.labelScore.text = this.score;
   },
 
 
-//Funció de reset
   restartGame: function() {
-
+    game.state.start('main');
   },
 
-//Game Over
+
   gameOver: function () {
 
 
@@ -165,11 +166,6 @@ var mainState = {
 
 };
 
-
-
-// Initialize Phaser, and create a 400px by 500px game.
-//El quart paràmetre defineix el div on pintarem el joc al nostre laravel.
 var game = new Phaser.Game(400, 500, Phaser.AUTO, 'doodle-jump');
 
-// Add and start the 'main' state to start the game
 game.state.add('main', mainState, true);
